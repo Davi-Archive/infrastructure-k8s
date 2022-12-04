bd5ef321c41aa6dac6266858ab39e9c9a14c87e6
d1av <davi4alves@gmail.com>
2022-12-04 13:33:03 -0300

    feat: add complete app deployment

---
+++ kubernetes5/script.bat
@@ -0,0 +1,20 @@
+echo "Criando as imagens...."
+
+docker build -t davi38/projeto-dio-backend:1.0 backend/.
+echo "backend imagem criada"
+
+docker build -t davi38/projeto-dio-database:1.0 database/.
+echo "database imagem criada"
+
+echo "Levando imagens ao  dockerpush"
+docker push davi38/projeto-dio-backend:1.0
+docker push davi38/projeto-dio-database:1.0
+ echo "Pushed images."
+
+ echo "Criando serviçoes no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments....."
+
+kubectl apply -f ./deployment.yml
